18ee4088ba4b009e3acacea57fb3a587d10202d5
Vinicius Pietscher <8095975+viniciuspietscher@users.noreply.github.com>
2022-09-15 20:01:26 -0400

    week15 - start

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"))
 app.use(express.json())
 const people = []
 
+app.post("/api/v1/people", (req, res) => {
+  const { name, age } = req.body
+  people.push([name, age])
+  // res.json({ error: "That route is not implemented." })
+  res.json({ msg: "person added" })
+})
 
-app.all('/api/v1/*', (req, res) => {
-    res.json({ error: 'That route is not implemented.' })
+app.get("/api/v1/people", (req, res) => {
+  res.json({ people: people })
+})
+
+app.get("/api/v1/people/:id", (req, res) => {
+  res.json({ person: people[req.params.id] })
 })
 
 app.listen(3000, () => {
-    console.log("listening on port 3000...")
+  console.log("listening on port 3000...")
 })
